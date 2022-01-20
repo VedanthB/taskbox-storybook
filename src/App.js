0f2c0d4ffac950e6c9b19091b7f79626a12b3934
Vedanth Bora <vedanthbora@gmail.com>
2022-01-21 02:35:07 +0530

    github ci action setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import store from "./lib/store";
 
 import { Provider } from "react-redux";
 import { InboxScreen } from "./components/InboxScreen";
-
 function App() {
   return (
     <Provider store={store}>
